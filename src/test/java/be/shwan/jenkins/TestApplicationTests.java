523fa8034e94cadde9eb12d21da50a26b45e91b6
unknown <20210476@I-BCCARD.BIZ>
2022-01-07 10:51:18 +0900

    init

---
+++ src/test/java/be/shwan/jenkins/TestApplicationTests.java
@@ -0,0 +1,13 @@
+package be.shwan.jenkins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
